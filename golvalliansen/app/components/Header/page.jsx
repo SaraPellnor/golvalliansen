2e642369c26a6bac3ed65bbdf1663021956f72e4
SaraPellnor <sara.ahlquist@hotmail.com>
2024-09-24 14:00:37 +0200

    scroll effect header

--- golvalliansen/app/components/Header/page.jsx
+++ golvalliansen/app/components/Header/page.jsx
@@ -1,10 +1,29 @@
+"use client"
 import logo from "@/public/assets/logo.png"
 import Image from "next/image"
 import HeaderMenu from "../HeaderMenu/page"
+import { useEffect, useState } from "react"
 
 const Header = () => {
-    return <div className=" flex flex-row items-center justify-between mx-10 h-44">
-        <Image src={logo} width={250} height={100} />
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        console.log("window.scrollY", window.scrollY);
+        const handleScroll = () => {
+            window.scrollY > 50 ? setIsScrolled(true) : setIsScrolled(false)
+            console.log(window.scrollY);
+        }
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [])
+
+    return <div className={`fixed flex flex-row items-center justify-between bg-white w-full transition-all duration-500 ease-in-out 
+    ${isScrolled ? "shadow-lg h-28" : "h-48"}`}>
+        <Image className={`transition-all duration-500 ease-in-out 
+            ${isScrolled && "w-1/6"}`} src={logo} width={250} height={100} alt="logo" />
         <HeaderMenu /> </div>
 }
 
---
+++ golvalliansen/app/pages/contact.js
@@ -0,0 +1,7 @@
+const Contact = () => {
+  return <div>
+    HEJ
+  </div>;
+};
+
+export default Contact;
